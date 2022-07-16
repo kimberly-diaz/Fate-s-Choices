bd97b707dd288ea63119d538f3807f2bba9ba806
kimberly-diaz <kimberly.diaz19@bathspa.ac.uk>
2022-07-16 09:20:40 +0400

    Uploaded files

---
+++ javascript/audio.js
@@ -0,0 +1,23 @@
+﻿//Audio
+var audio = document.getElementById("audio");
+
+//Play button
+function playbutton() {
+    audio.play();
+}
+
+//Pause button
+function pausebutton() {
+    audio.pause();
+}
+
+//Audio autoplays when user clicks on the play button
+var autoplay = false;
+onclick = function () {
+    if (!autoplay) {
+        document.querySelectorAll('audio').forEach(audio => {
+            audio.play();
+        });
+        autoplay = true;
+    }
+}
